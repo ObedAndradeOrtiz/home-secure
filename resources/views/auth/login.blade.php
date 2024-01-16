b193c51910c2d6a87c6dc7cd1741e71e1516a0f9
ONIJLOM <92702353+ONIJLOM@users.noreply.github.com>
2024-01-16 13:45:21 -0400

    login
    se saco el welcome y se puso directo el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,11 +6,19 @@
 
         <x-validation-errors class="mb-4" />
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+          @if (Route::has('register'))
+        <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+            @auth
+                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
